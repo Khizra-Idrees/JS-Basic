649e9985662f7f14c5bf66082df030c810e0042f
Khizra-Idrees <idreesmemon177@gmail.com>
2023-12-09 12:45:49 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,166 @@
+// ASIGNMENT 01
+// The sum of two numbers
+document.write("ASSIGNMENT 01");
+document.write("<br>");
+document.write("<br>");
+var a = 15
+var b = 12
+// Addition Operator
+document.write("ADDITION OPERATOR");
+document.write("<br>");
+document.write("a+b = 15+12=");
+
+document.write(a+b);
+document.write("<br>");
+document.write("<br>");
+
+// Subtraction Operator
+document.write("SUBTRACTION OPERATOR");
+document.write("<br>");
+document.write("a-b = 15-12=");
+
+document.write(a-b);
+document.write("<br>");
+document.write("<br>");
+
+// Multiplication Operator
+document.write("MULTIPLICATION OPERATOR");
+document.write("<br>");
+document.write("a*b = 15*12=");
+
+document.write(a*b);
+document.write("<br>");
+document.write("<br>");
+
+// Division Operator
+document.write("DIVISION OPERATOR");
+document.write("<br>");
+document.write("a/b = 15/12=");
+
+document.write(a/b);
+document.write("<br>");
+document.write("<br>");
+
+// Modulus Operator
+document.write("MODULUS OPERATOR");
+document.write("<br>");
+document.write("a%b = 15%12=");
+
+document.write(a%b);
+document.write("<br>");
+
+document.write("<br>");
+document.write("<br>");
+
+// ASIGNMENT 2
+document.write("ASSIGNMENT 02")
+document.write("<br>");
+document.write("<br>");
+
+// Operator Precedence
+document.write("OPERATOR PRECEDENCE")
+document.write("<br>");
+/* Operator Precedence mtlb agr kisi equation mai zyda operator h tu kis ka operation pehly hoga..Wese tu JS left to right operate krti h pr yhn pr BODMAS Rule apply hoga*/
+
+document.write((2+3*4/2-1))
+document.write("<br>");
+
+document.write("<br>");
+document.write("<br>");
+
+// ASSIGNMENT 03
+document.write("ASSIGNMENT 03")
+document.write("<br>");
+document.write("<br>");
+// Assign Operators
+document.write("ASSIGN OPERATOR")
+document.write("<br>");
+
+document.write("Add")
+document.write("<br>");
+var x=5;
+x+=3;
+document.write(x);
+
+document.write("<br>");
+document.write("<br>");
+
+document.write("Subtract")
+document.write("<br>");
+var x=5;
+x-=3;
+document.write(x);
+
+document.write("<br>");
+document.write("<br>");
+
+document.write("Mutiply")
+document.write("<br>");
+var x=5;
+x*=3;
+document.write(x);
+
+document.write("<br>");
+document.write("<br>");
+
+document.write("Divide")
+document.write("<br>");
+var x=5;
+x/=3;
+document.write(x);
+
+document.write("<br>");
+document.write("<br>");
+
+// ASSIGNMENT 04
+document.write("ASSIGNMENT 04")
+document.write("<br>");
+document.write("<br>");
+
+// Increament Operator
+document.write("Increament Operator")
+document.write("<br>");
+
+var count=10;
+count++;
+
+document.write(count);
+document.write("<br>");
+
+// Decreament Operator
+document.write("Decreament Operator")
+document.write("<br>");
+
+var count=10;
+count--;
+
+document.write(count);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
